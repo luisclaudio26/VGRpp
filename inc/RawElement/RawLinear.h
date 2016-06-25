7f08e8fd8a22992eef93afb38faea10685ba2541
Luís Cláudio G. Rocha <luisclaudiogouveiarocha@gmail.com>
2016-06-25 14:59:09 -0300

    + linear gradient. Not working yet!

--- inc/RawElement/RawLinear.h
+++ inc/RawElement/RawLinear.h
@@ -3,6 +3,7 @@
 
 #include "RawPaint.h"
 #include "../element/linear.h"
+#include "../spread.h"
 #include "../vector/vector2.h"
 #include "../types.h"
 #include <vector>
@@ -37,7 +38,19 @@ public:
 	//----- From RawPaint.h -----
 	Paint* preprocess(const Matrix3& xf, const Matrix3& scene_t) override
 	{
-		return new Linear();
+		//Transform points to canonical space:
+		//p0 at origin, p1 lying in x axis. Translate
+		//p0 and p1 to origin and rotate so p1 to lies on x axis
+		Matrix3 p0_to_origin = Matrix3::translate( -p0 );
+
+		double cosTheta = p1.x() / p1.norm();
+		double sinTheta = sqrt(1 - cosTheta*cosTheta);
+		Matrix3 rotate_p1 = Matrix3::affine(cosTheta, sinTheta, 0.0, -sinTheta, cosTheta, 0.0);
+		Matrix3 world_to_canonical = rotate_p1 * p0_to_origin * xf.inv() * scene_t.inv();
+
+		p1 = world_to_canonical.apply( p1.homogeneous() ).euclidean();
+
+		return new Linear(world_to_canonical, stops, spread_func_from_str(spread), p1.y() );
 	}
 };
 
--- inc/element/linear.h
+++ inc/element/linear.h
@@ -2,14 +2,83 @@
 #define _LINEAR_H_
 
 #include "paint.h"
+#include "../types.h"
+#include "../spread.h"
+#include "../vector/matrix3.h"
+#include <vector>
+#include <utility>
+#include "../vector/numeric.h"
 
 class Linear : public Paint
 {
 private:
+	std::vector<std::pair<double,Color_v> > stops;
+	spread_func spr_func;
+	Matrix3 world2canonical;
+	double grad_length;
+
+	void get_stop(double v, Color_v& s1, Color_v& s2)
+	{
+		std::pair<double,Color_v> cur, last;
+		int count = 1;
+		last = stops[count-1], cur = stops[count];
+
+		//TODO: This won't if v is outside interval [0,1]!
+		//Do error checking after
+		while( cur.first < v )
+		{
+			last = cur;
+			cur = stops[++count];
+		}
+
+		s1 = last.second; s2 = cur.second;
+	}
+
 public:
+	Linear(const Matrix3& world2canonical, const std::vector< std::pair<double,Color_v> >& stops, spread_func spr_func, double grad_length)
+	{
+		this->world2canonical = world2canonical;
+
+		//OMG! move semantics. I hope this is the right way to use it
+		//this should be safe because Stops in RawLinear object won't
+		//be used anymore.
+		this->stops = std::move(stops);
+
+		this->spr_func = spr_func;
+
+		this->grad_length = grad_length;
+	}
+
 	Color sample(double x, double y) override
 	{
-		return 0x00AABB00;
+		//transform point to canonical space
+		Vec2 cs_p = world2canonical.apply( Vec3(x,y,1.0) ).euclidean();
+
+		//get ratio of lengths ( ||cs_p|| / grad_length)
+		double v = cs_p.norm() / grad_length;
+		
+		//wrap it
+		v = spr_func(v);
+
+		//search corresponding stops
+		Color_v s1, s2;
+		get_stop(v, s1, s2);
+
+		//interpolate colors
+		Color_v out;
+		out.R = Numeric::lerp1(v, s1.R, s2.R);
+		out.G = Numeric::lerp1(v, s1.G, s2.G);
+		out.B = Numeric::lerp1(v, s1.B, s2.B);
+		out.A = Numeric::lerp1(v, s1.A, s2.A);
+
+		//compose final color
+		Color _color = 0;
+		_color += ( (Color)out.R << 24 );
+		_color += ( (Color)out.G << 16 );
+		_color += ( (Color)out.B << 8 );
+		_color += (Color)out.A;
+
+		return _color;
 	}	
 };
 
