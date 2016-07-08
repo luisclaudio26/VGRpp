0bbc4d9eb561bb32655eec4149683e2aee87efe0
Luís Cláudio G. Rocha <luisclaudiogouveiarocha@gmail.com>
2016-07-08 17:51:43 -0300

    erase code, write guidelines

--- src/element/triangle.cpp
+++ src/element/triangle.cpp
@@ -3,14 +3,23 @@
 
 #include <iostream>
 
+// Lembre-se que podemos desenhar o triângulo com os pontos
+// "existindo" no espaço da tela, i.e. aplicando xf e depois scene_xf
+// aos vértices e depois desenhando.
+
 //---------------------------------------
 //-------------- INTERNAL ---------------
 //---------------------------------------
 void compute_implicit_line(const Vec2& p0, const Vec2& p1, double* coef)
 {
-	double a = p1.y() - p0.y();
-	double b = p0.x() - p1.x();
-	double c = -a*p0.x() - b*p0.y();
+	// Aqui você calcula a, b e c para a equação de forma
+	// ax+by+c = 0 que passa por p0 e p1.
+
+	/*
+	double a = ...
+	double b = ...
+	double c = ...
+	*/
 
 	coef[0] = a;
 	coef[1] = b;
@@ -35,31 +44,58 @@ Triangle::Triangle(Vec2& p0, Vec2& p1, Vec2& p2)
 
 bool Triangle::is_inside(double x, double y)
 {
-	double edge_test[3];
-	for(int i = 0; i < 3; i++)
-		edge_test[i] = Numeric::sign( edge[i*3] * x + edge[i*3+1] * y + edge[i*3+2] );
-
-	return edge_test[0] == edge_test[1] && edge_test[1] == edge_test[2];
+	// Aqui você retorna VERDADEIRO ou FALSO se o ponto
+	// (x,y) está dentro ou fora do triângulo. Para isso,
+	// verifique se (x,y) está DO MESMO LADO* de todas as
+	// retas.
+	//
+	// * "Do mesmo lado" significa que, orientando as retas
+	// (definindo onde o segmento reta "começa" e "termina")
+	// em sentido horário/anti-horário, o ponto vai estar à
+	// à direita ou à esquerda de todas as retas ao mesmo tempo.
 }
 
 void Triangle::setxf(const Matrix3& xf)
 {
+	//Cada vez que a gente muda a transformação do MODELO, temos que
+	//recalcular onde os pontos estarão na tela.
 	this->xf = xf;
 	implicitize();
 }
 
 void Triangle::set_scenexf(const Matrix3& scenexf)
 {
+	//Cada vez que a gente muda a transformação da CENA, temos
+	//que recalcular onde os pontos estarão na tela.
 	this->scenexf = scenexf;
 	implicitize();
 }
 
 void Triangle::implicitize()
 {
-	Matrix3 t = scenexf * xf;
-	Vec2 p0_t = t.apply( p0.homogeneous() ).euclidean();
-	Vec2 p1_t = t.apply( p1.homogeneous() ).euclidean();
-	Vec2 p2_t = t.apply( p2.homogeneous() ).euclidean();
+	// Aqui você deve aplicar a transformação do modelo
+	// aos vértices, depois a transformação da cena
+	//
+	// Não esqueça que as transformações são matrizes 3x3
+	// e portanto devem ser aplicados a pontos HOMOGÊNEOS,
+	// que depois são "convertidos" de volta para pontos euclidianos.
+	//
+	// Lembre-se de não transformar os pontos p0, p1, p2 diretamente,
+	// pois se mudamos a transformação scene_xf ou xf precisaremos
+	// "lembrar" onde os pontos estavam originalmente (isto é, precisamos
+	// saber as coordenadas dos pontos tal como foram definidos no arquivo .2d)
+	
+	/*
+	Vec2 p0_t = ...
+	Vec2 p1_t = ...
+	Vec2 p2_t = ...
+	*/
+
+	// Uma vez que os pontos foram transformados, podemos
+	// calcular a equação implícita para cada uma das arestas.
+	// No cabeçalho triangle.h nós definimos um vetor double edge[9],
+	// que deverá armazenar os três parâmetros (a, b, c, de ax + by + c = 0)
+	// de cada uma das três arestas.
 
 	compute_implicit_line(p0_t, p1_t, edge);
 	compute_implicit_line(p1_t, p2_t, &edge[3]);
