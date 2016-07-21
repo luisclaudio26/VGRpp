288ba40371d1f7bc8bb7e4cb9caa9f6cdaaccc53
Luís Cláudio G. Rocha <luisclaudiogouveiarocha@gmail.com>
2016-07-20 22:55:27 -0300

    solve radial gradient

--- src/RawElement/RawRadial.cpp
+++ src/RawElement/RawRadial.cpp
@@ -19,7 +19,25 @@ Paint* RawRadial::preprocess(const Matrix3& paint_xf, const Matrix3& scene_xf)
 	// que mapeia um ponto para um espaço de gradiente radial canônico:
 	// Translade o foco para a origem, rotacione para por o centro no eixo x,
 	// escalone para o raio ser 1. Novamente, não esqueça da transformação do paint!
-	Matrix3 canonical_grad = Matrix3::identity();
+
+	// Translade o foco para a origem
+	Matrix3 to_origin = Matrix3::translate( -focus );
+
+	center = to_origin.apply( center.homogeneous() ).euclidean();
+
+	// Escale para que o círculo tenha raio 1
+	double s = 1.0 / radius;
+	Matrix3 to_unit = Matrix3::scale(s, s);
+
+	center = to_unit.apply( center.homogeneous() ).euclidean();
+
+	// Rotacione para o centro ir pro eixo x
+	double length = center.norm();
+	double cosTheta = center.x() / length, sinTheta = center.y() / length;
+	Matrix3 to_x_axis = Matrix3::affine(cosTheta, -sinTheta, 0.0, sinTheta, cosTheta, 0.0);
+
+	// Junte tudo e inclua as transformações de paint e cena
+	Matrix3 canonical_grad = to_x_axis * to_unit * to_origin * (scene_xf * paint_xf).inv();
 
 	//ATENÇÃO: Eu não sei exatamente o que o SVG/PS especifica para o caso em que o foco
 	//cai fora do círculo. Vamos só assumir que se ele estiver na borda, vamos trazê-lo
--- src/element/radial.cpp
+++ src/element/radial.cpp
@@ -22,7 +22,32 @@ ColorRGBA Radial::sample(double x, double y)
 	// e do tamanho do vetor (x,y). O resto é igual ao gradiente linear:
 	// aplique a função spr_func, procure o valor na rampa, interpole as
 	// cores, etc;
-	Color_v out = {1.0, 1.0, 1.0, 1.0};
+	Vec2 p = canonical_grad.apply( Vec3(x,y,1.0) ).euclidean();
+
+	// Compute interseção da reta que vai da origem até (x,y) com
+	// o círculo
+	double a = p.dot(p);
+	double b = -2.0*center*p.x();
+	double c = center*center - 1;
+
+	// Aparecerão duas raízes; uma delas será negativa e uma positiva;
+	// estamos interessados na positiva apenas.
+	double r1, r2;
+	Numeric::quadratic(a, b, c, r1, r2);
+	double r = (r1 > r2) ? r1 : r2;
+
+	// A razão entre o comprimento da reta que vai da origem até P
+	// e da reta que vai da origem à interseção é 1/r
+	double v = 1.0 / r;
+
+	// "Envelope" o valor
+	v = spread(v);
+
+	// Amostre a rampa de cor
+	Color_v c1, c2;
+	double t = Color::get_stop(v, c1, c2, stops);
+
+	Color_v out = Color::color_lerp(t, c1, c2);
 
 	return Color::rgba_from_colorv(out);
 }
